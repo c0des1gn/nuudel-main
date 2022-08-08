83082796e0994942fbf1f08ca2a40478200e8f95
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-08-08 18:00:57 +0800

    getHash

--- src/lib/common/helpers.ts
+++ src/lib/common/helpers.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto';
+
 export const encBase64 = (
   data: string,
   encoding: BufferEncoding = 'utf8'
@@ -13,4 +15,16 @@ export const decBase64 = (data: string, encoding: string = 'utf8'): string => {
     return '';
   }
   return Buffer.from(data, 'base64').toString(encoding);
+};
+
+export const getHash = (
+  divider: number = 100000,
+  length: number = 0
+): string => {
+  let qid: number = Math.ceil(Date.now() / divider);
+  const hash: string = crypto
+    .createHash('sha256')
+    .update(qid.toString())
+    .digest('hex');
+  return length > 0 ? hash.substring(0, length) : hash;
 };
