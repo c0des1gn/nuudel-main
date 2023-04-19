938fa9e860eaf953b22cddaa49167056e3122c6a
Damdinsuren Dorjsuren <damdinsuren@live.com>
2023-04-19 09:34:22 +0800

    upload

--- src/lib/controller/aws-s3.ts
+++ src/lib/controller/aws-s3.ts
@@ -1,5 +1,12 @@
+// https://docs.digitalocean.com/products/spaces/reference/s3-sdk-examples/
 import fs from 'fs';
-import S3, { PutObjectRequest } from 'aws-sdk/clients/s3';
+import {
+  PutObjectCommand,
+  PutObjectCommandInput,
+  DeleteObjectCommand,
+  //S3Client,
+  S3,
+} from '@aws-sdk/client-s3';
 import { folder } from './upload';
 import { imageResize } from './imageResize';
 
@@ -7,31 +14,30 @@ const {
   DOMAIN,
   OBJECT_STORAGE_KEY_ID,
   OBJECT_STORAGE_SECRET,
-  NEXT_PUBLIC_IMAGE_UPLOAD_URL,
+  NEXT_PUBLIC_IMAGE_UPLOAD_URL = 'nyc3.digitaloceanspaces.com', // s3.us-east-1.amazonaws.com
   NEXT_PUBLIC_OBJECT_STORAGE_BUCKET,
   AWS_DEFAULT_REGION = 'us-east-1',
-} = process.env;
+} = process?.env;
 
 let s3: S3 = new S3({
-  endpoint: `https://${
-    NEXT_PUBLIC_IMAGE_UPLOAD_URL || 'nyc3.digitaloceanspaces.com' // s3.us-east-1.amazonaws.com
-  }`,
+  endpoint: `https://${NEXT_PUBLIC_IMAGE_UPLOAD_URL}`,
   region: AWS_DEFAULT_REGION,
+  //apiVersion: '2010-12-01',
   credentials: {
     accessKeyId: OBJECT_STORAGE_KEY_ID,
     secretAccessKey: OBJECT_STORAGE_SECRET,
   },
-  s3ForcePathStyle: true,
+  forcePathStyle: false, // Configures to use subdomain/virtual calling format.
 });
 
 export async function push(
-  path: string,
+  filename: string,
   encoding: string,
   mimetype: string,
   toWidth?: number
 ): Promise<string> {
   return new Promise<string>(async (resolve, reject) => {
-    let filepath: string = folder + path;
+    let filepath: string = folder + filename;
     if (!fs.existsSync(filepath)) {
       reject('File ' + filepath + ' does not exist');
     }
@@ -45,46 +51,69 @@ export async function push(
       fileContent = fs.readFileSync(filepath); //fs.createReadStream(path)
     }
 
+    const path =
+      DOMAIN +
+      '/' +
+      new Date().getFullYear().toString() +
+      '/' +
+      (new Date().getMonth() + 1).toString().padStart(2, '0') +
+      '/';
     // Setting up S3 upload parameters
-    const params: PutObjectRequest = {
+    const params: PutObjectCommandInput = {
       Bucket: NEXT_PUBLIC_OBJECT_STORAGE_BUCKET,
-      Key:
-        DOMAIN +
-        '/' +
-        new Date().getFullYear().toString() +
-        '/' +
-        (new Date().getMonth() + 1).toString().padStart(2, '0') +
-        '/' +
-        path,
+      Key: path + filename,
       Body: fileContent,
       ContentEncoding: encoding,
       ContentType: mimetype,
       ACL: 'public-read',
-      //GrantFullControl: 'READ', // 'READ_ACP'
     };
 
     // Uploading files to the bucket
     try {
-      s3.upload(params, function (err, data) {
-        if (err || !data) {
-          return reject('');
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-        resolve(data.Location);
-      });
+      const data = await s3.send(new PutObjectCommand(params));
+      if (!data) {
+        return reject('');
+      }
+      let url: string = replaceSpace(
+        `https://${NEXT_PUBLIC_OBJECT_STORAGE_BUCKET}.${NEXT_PUBLIC_IMAGE_UPLOAD_URL}/${path}${encodeURIComponent(
+          filename
+        )}`
+      );
+      console.log(
+        `File uploaded successfully ${data?.$metadata?.requestId}: ${url}`
+      );
+      resolve(url);
     } catch (err) {
-      console.log(`PutObject =======>`, err);
+      console.log(`PutObject ====>`, err);
       return reject('');
     }
   });
 }
 
 export async function remove(filename: string) {
-  var params = { Bucket: NEXT_PUBLIC_OBJECT_STORAGE_BUCKET, Key: filename };
-
-  s3.deleteObject(params, function (err, data) {
-    if (err) console.log(err, err.stack);
-    // error
-    else console.log(); // deleted
+  if (!filename) {
+    return;
+  }
+  let path = filename.split('/');
+  let name = path.pop();
+  filename =
+    path.join('/') + '/' + decodeURIComponent(replaceSpace(name, true));
+  const params = new DeleteObjectCommand({
+    Bucket: NEXT_PUBLIC_OBJECT_STORAGE_BUCKET,
+    Key: filename,
   });
+  try {
+    const data = await s3.send(params);
+    console.log('Success. Object deleted.', data);
+    return data;
+  } catch (err) {
+    console.log('Error', err);
+  }
+}
+
+function replaceSpace(name, reverse: boolean = false) {
+  if (!NEXT_PUBLIC_IMAGE_UPLOAD_URL?.includes('.amazonaws.com')) {
+    return name;
+  }
+  return !reverse ? name.replace(/%20/g, '+') : name.replace(/+/g, '%20');
 }
