a8e3c51aa9a297cbd8dbba107d631a300c790f64
Damdinsuren Dorjsuren <damdinsuren@live.com>
2023-05-21 22:39:34 +0800

    fix

--- src/lib/common/helpers.ts
+++ src/lib/common/helpers.ts
@@ -10,11 +10,14 @@ export const encBase64 = (
   return Buffer.from(data, encoding).toString('base64');
 };
 
-export const decBase64 = (data: string, encoding: string = 'utf8'): string => {
+export const decBase64 = (
+  data: string,
+  encoding: BufferEncoding = 'utf8'
+): string => {
   if (!data) {
     return '';
   }
-  return Buffer.from(data, 'base64').toString(encoding as BufferEncoding);
+  return Buffer.from(data, 'base64').toString(encoding);
 };
 
 export const checkHash = (
